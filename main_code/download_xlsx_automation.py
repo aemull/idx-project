16cf4a2f5e64351d45c226e663858036407ec5b2
Kang Mull <152836095+aemull@users.noreply.github.com>
2025-04-23 13:19:53 +0700

    Delete main_code directory

--- main_code/download_xlsx_automation.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-url = "https://idx.co.id/Portals/0/StaticData/ListedCompanies/Corporate_Actions/New_Info_JSX/Jenis_Informasi/01_Laporan_Keuangan/02_Soft_Copy_Laporan_Keuangan//Laporan%20Keuangan%20Tahun%202024/TW2/ABDA/FinancialStatement-2024-II-ABDA.xlsx"
-headers = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/110.0"}
-
-response = requests.get(url, stream=True, headers=headers)
-print(response.status_code) 
-
-"https://idx.co.id/Portals/0/StaticData/ListedCompanies/Corporate_Actions/New_Info_JSX/Jenis_Informasi/01_Laporan_Keuangan/02_Soft_Copy_Laporan_Keuangan//Laporan%20Keuangan%20Tahun%202023/Audit/ACES/FinancialStatement-2023-Tahunan-ACES.xlsx"
